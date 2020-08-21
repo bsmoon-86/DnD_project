ca285bfd3af9e6197e9da8ea4d801a735ed68056
treefrog18 <keafz0829@gmail.com>
2020-08-21 15:54:50 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,17 @@ router.route("/sign_up").get(function (req, res, next) {
 });
 
 router.route("/sign_up2").get(function (req, res, next) {
-    console.log(id)
+  console.log(id);
   res.render("sign_up2");
+});
+
+app.post("/sign_up2", function (req, res) {
+  const p_num = req.body.p_num;
+  const name = req.body.name;
+  const password = req.body.password;
+  const division = req.body.division;
+  console.log(id);
+  console.log(password);
+  console.log(name);
+  console.log(division);
 });
