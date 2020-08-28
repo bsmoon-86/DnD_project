566b9c024ffdd9397e830e0b9f8f9c90543ed697
Byung-sun <ekflafg@naver.com>
2020-08-28 10:02:34 +0900

    0827 update

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require("express");
+const { route } = require("./signin");
 var router = express.Router();
 var app = express();
 
 router.get("/", function (req, res, next) {
+  res.render("master_menu");
+});
+
+router.route("/login").get(function (req, res, next) {
   res.render("login");
 });
 
-router.route("/").get(function (req, res, next) {
-  res.render("login");
+router.route("/c_email").get(function (req, res, next) {
+  res.render("c_email");
 });
 
 router.route("/sign_up").get(function (req, res, next) {
